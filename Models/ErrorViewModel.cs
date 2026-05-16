dc761c1706c3494971b16441688f323762eb6c20
camilo-villada <villajuan163@gmail.com>
2026-05-15 19:40:58 -0500

    prueba

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PruebaDespliegue.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
